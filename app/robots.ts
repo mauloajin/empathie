0e09e67df986c88651129aa350daa21824576084
mauloajin <mauloa32153@gmail.com>
2026-07-15 16:45:34 +0900

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,2 @@
 import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots { return { rules: { userAgent: "*", allow: "/" }, sitemap: "https://empathie-ten.vercel.app/sitemap.xml" }; }
-import type { MetadataRoute } from "next";
-export default function robots(): MetadataRoute.Robots { return { rules: { userAgent: "*", allow: "/" }, sitemap: "https://empathie.vercel.app/sitemap.xml" }; }
